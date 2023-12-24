c3e11a0f361c9014bdf5b086e3beb64f2d0a1226
ShrBox <shrbox@outlook.com>
2023-12-24 17:24:38 +0800

    fix: fix lots of things

--- xmake.lua
+++ xmake.lua
@@ -1,28 +1,45 @@
-add_rules("mode.debug", "mode.release")
+add_rules("mode.release")
 
 add_repositories("liteldev-xmake-repo https://github.com/LiteLDev/xmake-repo.git")
 
 -- Packages from xmake-repo
 add_requires("asio 1.28.0")
-add_requires("nlohmann_json v3.11.2")
 add_requires("openssl 1.1.1-t")
-add_requires("gsl v3.1.0")
 add_requires("simpleini v4.19")
 add_requires("toml++ v3.4.0")
-add_requires("magic_enum v0.8.1")
-add_requires("entt v3.11.1")
-add_requires("leveldb 1.23")
-add_requires("rapidjson v1.1.0")
 
 -- Packages from liteldev-xmake-repo
 add_requires("dyncall 1.4")
-add_requires("symbolprovider v1.1.0")
 add_requires("lightwebsocketclient v1.0.0")
 add_requires("threadpool v1.0.0")
 add_requires("scriptx v0.1.0")
-add_requires("levilamina v1.0.0")
+add_requires("levilamina develop")
 add_requires("fifo_map v1.0.0")
 add_requires("demangler v2.0.0")
+
+package("levilamina")
+    add_urls("https://github.com/LiteLDev/LeviLamina.git")
+
+    -- Dependencies from xmake-repo.
+    add_deps("entt 3.12.2")
+    add_deps("fmt 10.1.1")
+    add_deps("gsl 4.0.0")
+    add_deps("leveldb 1.23")
+    add_deps("magic_enum 0.9.0")
+    add_deps("nlohmann_json 3.11.2")
+    add_deps("rapidjson 1.1.0")
+
+    -- Dependencies from liteldev-repo.
+    add_deps("ctre 3.8.1")
+    add_deps("pcg_cpp 1.0.0")
+    add_deps("pfr 2.1.1")
+    add_deps("preloader 1.3.1")
+    add_deps("symbolprovider 1.1.0")
+    add_deps("bdslibrary 1.20.50.03")
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)
 
 target("LeviScript")
     set_kind("shared")
@@ -30,16 +47,16 @@ target("LeviScript")
     add_files("src/**.cpp")
     add_includedirs("src")
     add_defines(
-        "D",
-        "D_AMD64_",
-        "D_CONSOLE",
-        "D_WINDLL",
-        "D_UNICODE",
-        "DCPPHTTPLIB_OPENSSL_SUPPORT",
-        "DNDEBUG",
-        "DNOMINMAX",
-        "DSCRIPTX_BACKEND_TRAIT_PREFIX=../backend/${env SCRIPTX_BACKEND}/trait/Trait",
-        "DUNICODE",
+        "_WIN32_WINNT=0x0601",
+        "_AMD64_",
+        "_CONSOLE",
+        "_WINDLL",
+        "_UNICODE",
+        "CPPHTTPLIB_OPENSSL_SUPPORT",
+        "NDEBUG",
+        "NOMINMAX",
+        "SCRIPTX_BACKEND_TRAIT_PREFIX=../backend/${env SCRIPTX_BACKEND}/trait/Trait",
+        "UNICODE",
         "LLSE_BACKEND_${env LLSE_BACKEND}"
     )
     add_runenvs("LLSE_BACKEND", "LUA")
@@ -58,7 +75,8 @@ target("LeviScript")
         "asio",
         "entt",
         "leveldb",
-        "rapidjson"
+        "rapidjson",
+        "fmt"
     )
 
     -- Packages from liteldev-xmake-repo

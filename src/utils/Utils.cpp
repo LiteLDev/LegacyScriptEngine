9a081d690aa85cd2509eac03d45126225cec7f1b
Zijian Zhang <futrime@outlook.com>
2024-01-22 18:52:27 +0800

    style: format code

--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -74,8 +74,7 @@ unsigned long long GetCurrentTimeStampMS() {
         currentTime.wHour,
         currentTime.wDay,
         currentTime.wMonth - 1,
-        currentTime.wYear - 1900
-    };
+        currentTime.wYear - 1900};
 
     unsigned long long nNow = mktime(&currTm) * 1000 + currentTime.wMilliseconds;
     return nNow;

b07b94d0ba4fb608443a04f417c0e50e6e6a5790
ShrBox <53301243+ShrBox@users.noreply.github.com>
2024-02-04 14:48:41 +0800

    feat: fix RemoteCall completely (#19)

--- src/legacy/api/RemoteCallAPI.cpp
+++ src/legacy/api/RemoteCallAPI.cpp
@@ -1,31 +1,194 @@
-#include "api/APIHelp.h"
+#include "RemoteCallAPI.h"
+
+#include "api/BaseAPI.h"
+#include "api/BlockAPI.h"
+#include "api/BlockEntityAPI.h"
+#include "api/ContainerAPI.h"
+#include "api/EntityAPI.h"
+#include "api/ItemAPI.h"
 #include "api/LlAPI.h"
+#include "api/NbtAPI.h"
+#include "api/PlayerAPI.h"
 #include "engine/GlobalShareData.h"
 #include "engine/MessageSystem.h"
+#include "legacyapi/remotecall/RemoteCallAPI.h"
 #include "ll/api/service/ServerInfo.h"
 
+#include <map>
 #include <process.h>
+#include <sstream>
 #include <string>
 
 #define DEFAULT_REMOTE_CALL_NAME_SPACE "LLSEGlobal"
+#define logger                         lse::getSelfPluginInstance().getLogger()
+
+//////////////////// Remote Call ////////////////////
+
+RemoteCall::ValueType pack(Local<Value> value);
+RemoteCall::ValueType pack(Local<Object> value) {
+    // Player*, Actor*, ItemStack*, Block*, BlockActor*, Container*, Vec3, BlockPos, CompoundTag*
+    if (IsInstanceOf<PlayerClass>(value)) return PlayerClass::extract(value);
+    if (IsInstanceOf<EntityClass>(value)) return EntityClass::extract(value);
+    if (IsInstanceOf<ItemClass>(value)) return ItemClass::extract(value);
+    if (IsInstanceOf<BlockClass>(value)) {
+        auto block = EngineScope::currentEngine()->getNativeInstance<BlockClass>(value);
+        return block->get();
+    }
+    if (IsInstanceOf<BlockEntityClass>(value)) return BlockEntityClass::extract(value);
+    if (IsInstanceOf<ContainerClass>(value)) return ContainerClass::extract(value);
+    if (IsInstanceOf<FloatPos>(value)) {
+        auto pos = FloatPos::extractPos(value);
+        return std::make_pair(pos->getVec3(), pos->getDimensionId());
+    }
+    if (IsInstanceOf<IntPos>(value)) {
+        auto pos = IntPos::extractPos(value);
+        return std::make_pair(pos->getBlockPos(), pos->getDimensionId());
+    }
+    if (IsInstanceOf<NbtCompoundClass>(value)) return NbtCompoundClass::extract(value);
+    std::unordered_map<std::string, RemoteCall::ValueType> result;
+    for (auto& k : value.getKeyNames()) result.emplace(k, pack(value.get(k)));
+    return std::move(result);
+}
+RemoteCall::ValueType pack(Local<Array> value) {
+    std::vector<RemoteCall::ValueType> result;
+    for (size_t index = 0ULL, mEnd = value.size(); index < mEnd; ++index) {
+        result.push_back(pack(value.get(index)));
+    }
+    return std::move(result);
+}
+
+RemoteCall::ValueType pack(Local<Value> value) {
+    switch (value.getKind()) {
+    case script::ValueKind::kNull:
+        return nullptr;
+        break;
+    case script::ValueKind::kObject:
+        return pack(value.asObject());
+        break;
+    case script::ValueKind::kString:
+        return value.toStr();
+        break;
+    case script::ValueKind::kNumber: {
+        auto num = value.asNumber();
+        return RemoteCall::NumberType(num.toInt64(), num.toDouble());
+        break;
+    }
+    case script::ValueKind::kBoolean:
+        return value.asBoolean().value();
+        break;
+    case script::ValueKind::kFunction:
+        throw std::exception(fmt::format(__FUNCTION__ " - Unsupported Type: kFunction").c_str());
+        break;
+    case script::ValueKind::kArray:
+        return pack(value.asArray());
+        break;
+    case script::ValueKind::kByteBuffer:
+        throw std::exception(fmt::format(__FUNCTION__ " - Unsupported Type: kByteBuffer").c_str());
+        return value.asByteBuffer().describeUtf8();
+        break;
+    case script::ValueKind::kUnsupported:
+        throw std::exception(fmt::format(__FUNCTION__ " - Unsupported Type: kUnsupported").c_str());
+        break;
+    default:
+        throw std::exception(fmt::format(__FUNCTION__ " - Unsupported Type: Unknown").c_str());
+        break;
+    }
+    return RemoteCall::ValueType();
+}
+
+// Player*, Actor*, ItemStack*, Block*, BlockActor*, Container*, Vec3, BlockPos, CompoundTag*
+Local<Value> _extractValue(bool v) { return Boolean::newBoolean(v); };
+Local<Value> _extractValue(std::string v) { return String::newString(v); };
+Local<Value> _extractValue(std::nullptr_t v) { return Local<Value>(); };
+Local<Value> _extractValue(std::string* v) { return Local<Value>(); };
+Local<Value> _extractValue(Player* v) { return PlayerClass::newPlayer(v); };
+Local<Value> _extractValue(Actor* v) { return EntityClass::newEntity(v); };
+Local<Value> _extractValue(Block* v) { return BlockClass::newBlock(v, &BlockPos::ZERO, -1); };
+Local<Value> _extractValue(BlockActor* const& v) { return BlockEntityClass::newBlockEntity(v, -1); };
+Local<Value> _extractValue(Container* v) { return ContainerClass::newContainer(v); };
+Local<Value> _extractValue(RemoteCall::WorldPosType v) { return FloatPos::newPos(v.pos, v.dimId); };
+Local<Value> _extractValue(RemoteCall::BlockPosType v) { return IntPos::newPos(v.pos, v.dimId); };
+Local<Value> _extractValue(RemoteCall::BlockType v) {
+    return BlockClass::newBlock(v.get<Block const*>(), &v.blockPos, v.dimension);
+};
+Local<Value> _extractValue(RemoteCall::NumberType v) { return Number::newNumber(v.get<double>()); };
+Local<Value> _extractValue(RemoteCall::ItemType&& v) {
+    if (v.own) return ItemClass::newItem(v.tryGetUniquePtr().release(), false);
+    else return ItemClass::newItem(const_cast<ItemStack*>(v.ptr), false);
+};
+Local<Value> _extractValue(RemoteCall::NbtType&& v) {
+    if (v.own) return NbtCompoundClass::pack(v.tryGetUniquePtr());
+    else return NbtCompoundClass::pack(const_cast<CompoundTag*>(v.ptr), false);
+};
+
+Local<Value> extract(RemoteCall::ValueType&& val);
+
+Local<Value> extractValue(RemoteCall::Value&& val) {
+    static_assert(std::variant_size_v<RemoteCall::Value> == 13);
+    switch (val.index()) {
+#define Extra(index)                                                                                                   \
+    case index:                                                                                                        \
+        return _extractValue(std::move(std::get<index>(val)));
+        Extra(0);
+        Extra(1);
+        Extra(2);
+        Extra(3);
+        Extra(4);
+        Extra(5);
+        Extra(6);
+        Extra(7);
+        Extra(8);
+        Extra(9);
+        Extra(10);
+        Extra(11);
+        Extra(12);
+    default:
+        throw std::exception("Unsupported Type!");
+        break;
+    }
+};
+Local<Value> extractValue(std::vector<RemoteCall::ValueType>&& val) {
+    auto arr = Array::newArray();
+    for (auto& v : val) arr.add(extract(std::move(v)));
+    return arr;
+};
+Local<Value> extractValue(std::unordered_map<std::string, RemoteCall::ValueType>&& val) {
+    auto obj = Object::newObject();
+    for (auto& [k, v] : val) {
+        obj.set(k, extract(std::move(v)));
+    }
+    return obj;
+};
+
+Local<Value> extract(RemoteCall::ValueType&& val) {
+    static_assert(std::variant_size_v<RemoteCall::ValueType::Type> == 3);
+    switch (val.value.index()) {
+    case 0:
+        return extractValue(std::move(std::get<0>(val.value)));
+    case 1:
+        return extractValue(std::move(std::get<1>(val.value)));
+    case 2:
+        return extractValue(std::move(std::get<2>(val.value)));
+    default:
+        throw std::exception("Unsupported Type");
+        return Local<Value>();
+        break;
+    }
+}
 
 Local<Value> MakeRemoteCall(const string& nameSpace, const string& funcName, const Arguments& args) {
-    auto data = globalShareData->exportedFuncs.find(nameSpace + ":" + funcName);
-    if (data == globalShareData->exportedFuncs.end()) {
-        lse::getSelfPluginInstance().getLogger().error(
-            "Fail to import! Function [{}::{}] has not been exported!",
-            nameSpace,
-            funcName
-        );
-        lse::getSelfPluginInstance().getLogger().error("In plugin <{}>", ENGINE_OWN_DATA()->pluginName);
+    auto& func = RemoteCall::importFunc(nameSpace, funcName);
+    if (!func) {
+        logger.error("Fail to import! Function [{}::{}] has not been exported!", nameSpace, funcName);
+        logger.error("In plugin <{}>", ENGINE_OWN_DATA()->pluginName);
         return Local<Value>();
     }
 
-    std::vector<std::string> params;
+    std::vector<RemoteCall::ValueType> params;
     for (int i = 0; i < args.size(); ++i) {
-        params.emplace_back(ValueToJson(args[i]));
+        params.emplace_back(pack(args[i]));
     }
-    return JsonToValue(data->second.callback(std::move(params)));
+    return extract(func(std::move(params)));
 }
 
 bool LLSEExportFunc(
@@ -34,35 +197,52 @@ bool LLSEExportFunc(
     const string&          nameSpace,
     const string&          funcName
 ) {
-    std::string       exportName = nameSpace + ":" + funcName;
-    ExportedFuncData* funcData   = &(globalShareData->exportedFuncs)[exportName];
-    if (funcData->engine) return false;
-    funcData->engine         = engine;
-    funcData->func           = script::Global<Function>(func);
-    funcData->fromEngineType = LLSE_MODULE_TYPE;
-    funcData->callback       = [exportName](std::vector<std::string> params) -> std::string {
-        auto data = globalShareData->exportedFuncs.find(exportName);
-        if (data == globalShareData->exportedFuncs.end()) {
-            lse::getSelfPluginInstance().getLogger().error("Exported function \"{}\" not found", exportName);
-            return "";
-        }
-        auto engine = data->second.engine;
-        if ((ll::getServerStatus() != ll::ServerStatus::Running) || !EngineManager::isValid(engine)
+    // Putting script::Global value into lambda capture list may cause crash
+    // script::Global<Function> callback = script::Global<Function>(func);
+    std::string            identifier = nameSpace + "::" + funcName;
+    RemoteCall::CallbackFn cb         = [engine, identifier /*, scriptCallback = std::move(callback)*/](
+                                    std::vector<RemoteCall::ValueType> params
+                                ) -> RemoteCall::ValueType {
+        if (ll::getServerStatus() != ll::ServerStatus::Stopping || !EngineManager::isValid(engine)
             || engine->isDestroying())
             return "";
-        EngineScope                       enter(data->second.engine);
-        std::vector<script::Local<Value>> scriptParams;
-        for (auto& param : params) {
-            scriptParams.emplace_back(JsonToValue(param));
+        EngineScope enter(engine);
+        try {
+            auto iter = ENGINE_GET_DATA(engine)->exportFuncs.find(identifier);
+            if (iter == ENGINE_GET_DATA(engine)->exportFuncs.end()) {
+                logger.debug("");
+                return "";
+            }
+            auto                              scriptCallback = iter->second.callback.get();
+            std::vector<script::Local<Value>> scriptParams;
+            for (auto& param : params) {
+                scriptParams.emplace_back(extract(std::move(param)));
+            }
+            return pack(scriptCallback.call({}, scriptParams));
         }
-        return ValueToJson(data->second.func.get().call({}, scriptParams));
+        CATCH_WITHOUT_RETURN("Fail in Remote Call");
+        return "";
     };
-    return true;
+    if (RemoteCall::exportFunc(nameSpace, funcName, std::move(cb))) {
+        ENGINE_GET_DATA(engine)->exportFuncs.emplace(
+            identifier,
+            RemoteCallData{nameSpace, funcName, script::Global<Function>(func)}
+        );
+        return true;
+    }
+    return false;
 }
 
 bool LLSERemoveAllExportedFuncs(ScriptEngine* engine) {
-    globalShareData->exportedFuncs.clear();
-    return true;
+    // enter scope to prevent crash in script::Global::~Global()
+    EngineScope                                      enter(engine);
+    std::vector<std::pair<std::string, std::string>> funcs;
+    for (auto& [key, data] : ENGINE_GET_DATA(engine)->exportFuncs) {
+        funcs.emplace_back(data.nameSpace, data.funcName);
+    }
+    int count = RemoteCall::removeFuncs(std::move(funcs));
+    ENGINE_GET_DATA(engine)->exportFuncs.clear();
+    return count;
 }
 
 //////////////////// APIs ////////////////////
@@ -130,9 +310,8 @@ Local<Value> LlClass::hasFuncExported(const Arguments& args) {
             funcName  = args[0].toStr();
         }
 
-        return Boolean::newBoolean(
-            globalShareData->exportedFuncs.find(nameSpace + ":" + funcName) != globalShareData->exportedFuncs.end()
-        );
+        // 远程调用
+        return Boolean::newBoolean(RemoteCall::hasFunc(nameSpace, funcName));
     }
     CATCH("Fail in LLSEHasExported!")
 }

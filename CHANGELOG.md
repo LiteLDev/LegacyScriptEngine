3f5d08aa228a1fb03092a62f25c354697838f632
Futrime <futrime@outlook.com>
2024-02-05 08:58:26 +0800

    build: for 0.1.4 release

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.4] - 2024-02-05
+
+### Added
+
+- First release.
+
+[0.1.4]: https://github.com/LiteLDev/LegacyScriptEngine/releases/tag/v0.1.4
---
+++ scripts/validate_release.py
@@ -0,0 +1,81 @@
+import argparse
+import re
+import subprocess
+from typing import TypedDict
+
+
+class Args(TypedDict):
+    tag: str
+
+
+def main():
+    args = get_args()
+
+    version = args["tag"].lstrip("v")
+
+    validate_changelog(version)
+    validate_tooth_json(version)
+    validate_tooth_template_json(version)
+
+
+def get_args() -> Args:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tag", required=True)
+
+    args = parser.parse_args()
+
+    return {
+        "tag": args.tag,
+    }
+
+
+def validate_changelog(version: str):
+    try:
+        subprocess.run(
+            f"npx changelog --format markdownlint",
+            shell=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print("Have you installed it by `npm i -g keep-a-changelog`?")
+        raise e
+
+    with open("CHANGELOG.md", "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if not re.search(r"## \[{}\]".format(version), content):
+        raise Exception("CHANGELOG.md lacks version {}".format(version))
+
+
+def validate_tooth_json(version: str):
+    with open("tooth.json", "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if not re.search(r"\"version\": \"{}\"".format(version), content):
+        raise Exception("tooth.json has wrong version")
+
+    if not re.search(
+        r"\"gitea.litebds.com/LiteLDev/legacy-script-engine-lua\": \"{}\"".format(
+            version
+        ),
+        content,
+    ):
+        raise Exception("tooth.json has wrong version in gitea.litebds.com/LiteLDev/legacy-script-engine-lua")
+
+    if not re.search(
+        r"\"gitea.litebds.com/LiteLDev/legacy-script-engine-quickjs\": \"{}\"".format(
+            version
+        ),
+        content,
+    ):
+        raise Exception("tooth.json has wrong version in gitea.litebds.com/LiteLDev/legacy-script-engine-quickjs")
+
+def validate_tooth_template_json(version: str):
+    with open("tooth.template.json", "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if not re.search(r"\"version\": \"{}\"".format(version), content):
+        raise Exception("tooth.template.json has wrong version")
+
+if __name__ == "__main__":
+    main()

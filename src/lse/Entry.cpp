94c4a42b6ad28c38da1e16c111386f76f54d00a1
futrime <futrime@outlook.com>
2024-02-12 18:48:12 +0800

    refactor: avoid including troublesome legacy header in lse namespace

--- src/lse/Entry.cpp
+++ src/lse/Entry.cpp
@@ -3,24 +3,20 @@
 #include "Config.h"
 #include "PluginManager.h"
 #include "PluginMigration.h"
-#include "legacy/api/EventAPI.h"
 #include "legacy/api/MoreGlobal.h"
-#include "legacy/engine/GlobalShareData.h"
-#include "legacy/engine/LocalShareData.h"
-#include "legacy/engine/MessageSystem.h"
-#include "legacy/main/BuiltinCommands.h"
 #include "legacy/main/EconomicSystem.h"
-#include "legacy/main/Loader.h"
-#include "legacy/main/SafeGuardRecord.h"
 
 #include <fmt/format.h>
 #include <functional>
 #include <ll/api/Config.h>
+#include <ll/api/i18n/I18n.h>
 #include <ll/api/io/FileUtils.h>
 #include <ll/api/plugin/NativePlugin.h>
 #include <ll/api/plugin/PluginManagerRegistry.h>
 #include <memory>
 #include <stdexcept>
+#include <string>
+#include <unordered_map>
 
 #ifdef LEGACY_SCRIPT_ENGINE_BACKEND_LUA
 
@@ -33,6 +29,17 @@ constexpr auto BaseLibFileName = "BaseLib.lua";
 constexpr auto BaseLibFileName = "BaseLib.js";
 
 #endif
+
+// Do not use legacy headers directly, otherwise there will be tons of errors.
+void InitBasicEventListeners();
+void InitGlobalShareData();
+void InitLocalShareData();
+void InitMessageSystem();
+void InitSafeGuardRecord();
+void LoadDebugEngine();
+void RegisterDebugCommand();
+extern std::unordered_map<std::string, std::string>
+    depends; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables
 
 namespace lse {
 
@@ -52,7 +59,6 @@ auto load(ll::plugin::NativePlugin& self) -> bool;
 auto loadBaseLib(ll::plugin::NativePlugin& self) -> bool;
 
 extern "C" {
-
 _declspec(dllexport) auto ll_plugin_load(ll::plugin::NativePlugin& self) -> bool { return load(self); }
 
 _declspec(dllexport) auto ll_plugin_enable(ll::plugin::NativePlugin& self) -> bool { return enable(self); }

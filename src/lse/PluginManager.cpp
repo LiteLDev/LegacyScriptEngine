af94c7ebb626cd47561a51034ba3ae41c7f280cf
futrime <futrime@outlook.com>
2024-02-13 10:27:50 +0800

    refactor: simplify lambda

--- src/lse/PluginManager.cpp
+++ src/lse/PluginManager.cpp
@@ -5,6 +5,7 @@
 #include "legacy/main/PluginManager.h"
 
 #include <fmt/format.h>
+#include <ll/api/Logger.h>
 #include <ll/api/plugin/Plugin.h>
 #include <ll/api/plugin/PluginManager.h>
 #include <memory>
@@ -24,6 +25,37 @@ constexpr auto PluginManagerName = "lse-quickjs";
 
 namespace lse {
 
+namespace {
+
+auto loadPlugin(ll::plugin::Plugin& plugin, ll::Logger& logger) -> bool {
+    const auto& manifest = plugin.getManifest();
+
+    logger.info("loading plugin {}", manifest.name);
+
+    auto pluginDir = std::filesystem::canonical(ll::plugin::getPluginsRoot() / manifest.name);
+    auto entryPath = pluginDir / manifest.entry;
+
+    if (!::PluginManager::loadPlugin(entryPath.string(), false, true)) {
+        throw std::runtime_error(fmt::format("failed to load plugin {}", manifest.name));
+    }
+
+    return true;
+}
+
+auto unloadPlugin(ll::plugin::Plugin& plugin, ll::Logger& logger) -> bool {
+    auto pluginName = plugin.getManifest().name;
+
+    logger.info("unloading plugin {}", pluginName);
+
+    if (!::PluginManager::unloadPlugin(pluginName)) {
+        throw std::runtime_error(fmt::format("failed to unload plugin {}", pluginName));
+    }
+
+    return true;
+}
+
+} // namespace
+
 PluginManager::PluginManager() : ll::plugin::PluginManager(PluginManagerName) {}
 
 auto PluginManager::load(ll::plugin::Manifest manifest) -> bool {
@@ -35,31 +67,9 @@ auto PluginManager::load(ll::plugin::Manifest manifest) -> bool {
 
     auto plugin = std::make_shared<Plugin>(manifest);
 
-    plugin->onLoad([&logger](ll::plugin::Plugin& plugin) -> bool {
-        const auto& manifest = plugin.getManifest();
-
-        logger.info("loading plugin {}", manifest.name);
-
-        auto pluginDir = std::filesystem::canonical(ll::plugin::getPluginsRoot() / manifest.name);
-        auto entryPath = pluginDir / manifest.entry;
-
-        if (!::PluginManager::loadPlugin(entryPath.string(), false, true)) {
-            throw std::runtime_error(fmt::format("failed to load plugin {}", manifest.name));
-        }
-        return true;
-    });
-
-    plugin->onUnload([&logger](ll::plugin::Plugin& plugin) -> bool {
-        auto pluginName = plugin.getManifest().name;
-
-        logger.info("unloading plugin {}", pluginName);
-
-        if (!::PluginManager::unloadPlugin(pluginName)) {
-            throw std::runtime_error(fmt::format("failed to unload plugin {}", pluginName));
-        }
-
-        return true;
-    });
+    // Register callbacks.
+    plugin->onLoad([&logger](ll::plugin::Plugin& plugin) -> bool { return loadPlugin(plugin, logger); });
+    plugin->onUnload([&logger](ll::plugin::Plugin& plugin) -> bool { return unloadPlugin(plugin, logger); });
 
     if (!plugin->onLoad()) {
         throw std::runtime_error(fmt::format("failed to load plugin {}", manifest.name));

63afeb0d3a32a17cdb3db965466137ec9f7af00e
ShrBox <shrbox@outlook.com>
2024-03-28 08:54:45 +0800

    chore: update tooth.json & changelog.md

--- xmake.lua
+++ xmake.lua
@@ -9,7 +9,7 @@ add_requires(
     "legacymoney 0.5.0",
     "legacyparticleapi 0.5.0",
     "legacyremotecall 0.5.0",
-    "levilamina 0.10.1",
+    "levilamina 0.10.2",
     "lightwebsocketclient",
     "magic_enum",
     "nlohmann_json",

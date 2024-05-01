0f54950296c6b5e335b7215b6512a6d5eec7b49b
ShrBox <shrbox@outlook.com>
2024-05-01 13:13:35 +0800

    chore: format code

--- src/legacy/api/CommandAPI.cpp
+++ src/legacy/api/CommandAPI.cpp
@@ -173,7 +173,12 @@ Local<Value> McClass::runcmd(const Arguments& args) {
     CHECK_ARG_TYPE(args[0], ValueKind::kString)
     CommandContext context = CommandContext(
         args[0].asString().toString(),
-        std::make_unique<ServerCommandOrigin>("Server", ll::service::getLevel()->asServer(), CommandPermissionLevel::Owner, 0)
+        std::make_unique<ServerCommandOrigin>(
+            "Server",
+            ll::service::getLevel()->asServer(),
+            CommandPermissionLevel::Owner,
+            0
+        )
     );
     try {
         return Boolean::newBoolean(ll::service::getMinecraft()->getCommands().executeCommand(context, false));

8ac8212bb89ba2af1b58be6b04ae4e303213deb8
ShrBox <shrbox@outlook.com>
2025-02-28 17:53:24 +0800

    chore: update levilamina

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,11 @@
 add_rules("mode.debug", "mode.release")
 
-add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
 
 if is_config("target_type", "server") then
-    add_requires("levilamina eb0c96980a7f7f7a4d0340c84d9aca4b293aad4a", {configs = {target_type = "server"}})
+    add_requires("levilamina 2e818330ddc776bffa06c2ccea9f6f6497b8978d", {configs = {target_type = "server"}})
 else
-    add_requires("levilamina eb0c96980a7f7f7a4d0340c84d9aca4b293aad4a", {configs = {target_type = "client"}})
+    add_requires("levilamina 2e818330ddc776bffa06c2ccea9f6f6497b8978d", {configs = {target_type = "client"}})
 end
 
 add_requires("levibuildscript")

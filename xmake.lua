d413de5a3e4f9dacd086220d0e0b166f4621e896
ShrBox <shrbox@outlook.com>
2025-05-22 18:02:36 +0800

    chore: update ScriptX to fix v8 backend issue #277
    (cherry picked from commit 5273ecb813f1d8b74d625eb43cef6ca15cdbabb1)

--- xmake.lua
+++ xmake.lua
@@ -25,20 +25,20 @@ add_requires(
 if is_config("backend", "lua") then
     add_requires("openssl 1.1.1-w")
     add_requires("mariadb-connector-c 3.3.9")
-    add_requires("scriptx 775b9f5de5ded72956762aaf8708f2e46ddeb93c", {configs={backend="Lua"}})
+    add_requires("scriptx 22184aeecdcee2c683ad99db6362837e4ec6b7fe", {configs={backend="Lua"}})
 
 elseif is_config("backend", "quickjs") then
     add_requires("openssl 1.1.1-w")
     add_requires("mariadb-connector-c 3.3.9")
-    add_requires("scriptx 775b9f5de5ded72956762aaf8708f2e46ddeb93c", {configs={backend="QuickJs"}})
+    add_requires("scriptx 22184aeecdcee2c683ad99db6362837e4ec6b7fe", {configs={backend="QuickJs"}})
 
 elseif is_config("backend", "python") then
     add_requires("openssl 1.1.1-w")
     add_requires("mariadb-connector-c 3.3.9")
-    add_requires("scriptx 775b9f5de5ded72956762aaf8708f2e46ddeb93c", {configs={backend="Python"}})
+    add_requires("scriptx 22184aeecdcee2c683ad99db6362837e4ec6b7fe", {configs={backend="Python"}})
 
 elseif is_config("backend", "nodejs") then
-    add_requires("scriptx 775b9f5de5ded72956762aaf8708f2e46ddeb93c", {configs={backend="V8"}})
+    add_requires("scriptx 22184aeecdcee2c683ad99db6362837e4ec6b7fe", {configs={backend="V8"}})
 
 end
 
